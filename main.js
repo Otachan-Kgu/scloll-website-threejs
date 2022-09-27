aa6c200895fc73338125d1faf854597da214c39c
太田紘彰 <otahiroaki@ootahiroshiakiranoMacBook-Air.local>
2022-09-28 00:31:42 +0900

    ジオメトリの追加

--- main.js
+++ main.js
@@ -1,2 +1,68 @@
-import './style.css'
+import "./style.css";
 import * as THREE from "three";
+
+//canvas
+const canvas = document.querySelector("#webgl");
+
+//シーン
+const scene = new THREE.Scene();
+
+//背景用のテクスチャ
+const textureLoader = new THREE.TextureLoader();
+const bgTexture = textureLoader.load("bg/bg.jpg");
+scene.background = bgTexture;
+
+//サイズ
+const sizes = {
+  width: innerWidth,
+  height: innerHeight,
+};
+
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+
+//レンダラー
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+//オブジェクトの作成
+const boxGeometry = new THREE.BoxGeometry(5, 5, 5, 10);
+const boxMaterial = new THREE.MeshNormalMaterial();
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.set(0, 0.5, -15);
+box.rotation.set(1, 1, 0);
+
+const torusGeometry = new THREE.TorusGeometry(8, 2, 16, 100);
+const torusMaterial = new THREE.MeshNormalMaterial();
+const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+torus.position.set(0, 1, 10);
+
+scene.add(box, torus);
+
+//アニメーション
+const tick = () => {
+  window.requestAnimationFrame(tick);
+  renderer.render(scene, camera);
+};
+
+tick();
+
+//ブラウザのリサイズ操作
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
